79169ace553372cdeffd4ba7c54e612c5d26e240
Dev Royale <unityekeoba18@gmail.com>
2024-01-09 12:12:50 +0000

    updated authenticatiom key

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "@firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw8kPQkxNvVY2cB6ghVc20FlxhP4e_Xs4",
-  authDomain: "testproj-531d7.firebaseapp.com",
-  projectId: "testproj-531d7",
-  storageBucket: "testproj-531d7.appspot.com",
-  messagingSenderId: "705585244276",
-  appId: "1:705585244276:web:6905459d1c0800f7ba0921",
-  measurementId: "G-KWJSWQRNV0"
+  apiKey: "AIzaSyB0lwftO6dxR4DZuH3JPh-OYi0Q363uUc0",
+  authDomain: "chat-app-9ca07.firebaseapp.com",
+  projectId: "chat-app-9ca07",
+  storageBucket: "chat-app-9ca07.appspot.com",
+  messagingSenderId: "489223132432",
+  appId: "1:489223132432:web:aac1d542b84421bb588195",
+  measurementId: "G-9EP3ETQ05Q"
 };
 
 // Initialize Firebase
